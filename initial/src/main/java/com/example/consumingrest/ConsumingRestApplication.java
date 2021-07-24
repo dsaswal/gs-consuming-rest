b9f42f0e51893a828523155746c9d676ffa88eea
dsaswal from ec2 <ec2-user@ip-172-31-33-237.us-east-2.compute.internal>
2021-07-24 22:55:42 +0000

    Read REST endpoint from property file

--- initial/src/main/java/com/example/consumingrest/ConsumingRestApplication.java
+++ initial/src/main/java/com/example/consumingrest/ConsumingRestApplication.java
@@ -3,6 +3,8 @@ package com.example.consumingrest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -10,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@PropertySource({ "classpath:application.properties" })
 public class ConsumingRestApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(ConsumingRestApplication.class);
--- initial/src/main/java/com/example/consumingrest/Quote.java
+++ initial/src/main/java/com/example/consumingrest/Quote.java
@@ -9,13 +9,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
 @RestController
 @RequestMapping("/quotes")
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Configuration
+@PropertySource("classpath:application.properties")
 public class Quote {
 
+  private static final Logger log = LoggerFactory.getLogger(Quote.class);
   private String type;
   private Value value;
+
+	@org.springframework.beans.factory.annotation.Value("${RANDOM_REST_URL}")
+	String RANDOM_REST_URL;
 
   public Quote() {
   }
@@ -46,8 +57,11 @@ public class Quote {
 
   @GetMapping("/quote") 
   public String getQuoteFromAPI() {
+	//@org.springframework.beans.factory.annotation.Value(" ${RANDOM_REST_URL} ")
+	//String RANDOM_REST_URL;
+	  log.info("RANDOM_REST_URL " + RANDOM_REST_URL);
 	  RestTemplate  restTemplate = new RestTemplate();
-	  Quote theQuote = restTemplate.getForObject("https://quoters.apps.pcfone.io/api/random", Quote.class);
+	  Quote theQuote = restTemplate.getForObject(RANDOM_REST_URL, Quote.class);
 	  //return theQuote.toString();
 	  //return theQuote.getValue().toString();
 	  return theQuote.getValue().getQuote();
